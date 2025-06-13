a09bcbbcef41ee9cd2700869dcb3003052656044
VoidArc Studio <chudybyczek2137@gmail.com>
2025-06-13 14:47:03 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt install python3-flask
+
+#clonned repo
+cd /HackeOS-TV
+python3 -m venv venv
+source venv/bin/activate
+pip install flask-wtf #if not installed flask use pip install flask
+#start-app 
